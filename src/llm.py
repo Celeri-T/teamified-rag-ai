6410526d0b5bd960878f66d0aaa2ea2a028ff815
Celeri-T <tmagpantay@outlook.up.edu.ph>
2026-04-10 21:04:59 +0800

    fix: make generation configs explicit to prevent error message

--- src/llm.py
+++ src/llm.py
@@ -20,16 +20,19 @@ def get_llm() -> HuggingFacePipeline:
         device_map="auto",
     )
 
+    # Set Generation Config
+    model.generation_config.max_new_tokens = 256
+    model.generation_config.do_sample = True
+    model.generation_config.temperature = 0.1
+    model.generation_config.top_p = 0.9
+    model.generation_config.repetition_penalty = 1.2
+    model.generation_config.pad_token_id = tokenizer.eos_token_id
+
     # Transformers Pipeline
     hf_pipeline = pipeline(
         "text-generation",
         model=model,
         tokenizer=tokenizer,
-        max_new_tokens=150,
-        temperature=0.1,
-        top_p=0.9,
-        repetition_penalty=1.2,
-        do_sample=True,
         return_full_text=False,
     )
 
